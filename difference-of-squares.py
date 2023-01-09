b2d058500b2e3ccf7e0d329e03f4962c7441a340
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2023-01-09 07:50:43 -0500

    Fixed

--- difference-of-squares.py
+++ difference-of-squares.py
@@ -10,11 +10,3 @@
 
 # Your final result must be printed as a formatted sentence exactly
 # the same as my answer in the answers.txt file.
-
-sum_squares = 0
-squares_sum = 0
-for i in range(1, 101):
-    sum_squares += i ** 2
-    squares_sum += i
-squares_sum **= 2
-print(squares_sum - sum_squares)
